35bdb4e94a73acee8adf576731cfdb5fe2ed47e4
danilolimacomp@gmail.com <djkcond@gmail.com>
2023-09-13 20:25:27 -0300

    atualização com base no que foi feito em sala

--- 18-InterfacePython/quedalivre/quedalivre.ino
+++ 18-InterfacePython/quedalivre/quedalivre.ino
@@ -1,8 +1,12 @@
+/*ESTE PROGRAMA DEU CERTO COM O CIRCUITO DO CAUÃ
+ * Arquivo disponível em 
+ * https://github.com/ProfessorDanilo/2023/tree/main/18-InterfacePython
+ */
 /*declarando as variaveis*/
 long tempo_i = 0;
 long tempo_f = 0;
-int eletroima = 12;//mudar para 4
-int botao = A1;
+int eletroima = 5;//mudar para 4
+int botao = 6;
 int liga = 0, desliga = 1;//mude a depender de como ligar o relé
 int led = 13;
 
@@ -15,22 +19,25 @@ void setup(void)
   pinMode(botao, INPUT_PULLUP);
   pinMode(led, OUTPUT);
   digitalWrite(eletroima, desliga);
-  digitalWrite(led, desliga);
+  digitalWrite(led, LOW);
 }
 
 void loop(void)
 {
   //veja que fica travado até que o botão seja pressionado
-  while(digitalRead(botao)){};
+  while(digitalRead(botao));
   delay(100);
-  digitalWrite(eletroima, liga);
+  digitalWrite(eletroima, liga);  
+  digitalWrite(led, HIGH);
   //Serial.print("eletroima_ligado");
   delay(5000);  
-  //Serial.print("eletroima_desligado");
+  //Serial.print("eletroima_desligado");  
+  digitalWrite(led, LOW);
   digitalWrite(eletroima, desliga);
+  delay(10);//Cauã, este é aquele Delay que você falou
   tempo_i = micros();
-  while(digitalRead(botao)){};
+  while(digitalRead(botao));
   tempo_f = micros();
-  Serial.println(tempo_f - tempo_i);
+  Serial.println(tempo_f - tempo_i+10000);
   delay(5000);
 }
