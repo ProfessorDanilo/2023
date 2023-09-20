268568fe21ae15c1027af4fc258e638af689076b
danilolimacomp@gmail.com <djkcond@gmail.com>
2023-09-20 20:58:50 -0300

    programa seguidor de linha: versão inicial

---
+++ 19-SeguidorDeLinha/19-SeguidorDeLinha.ino
@@ -0,0 +1,103 @@
+/*seguinto o material apresentado em aul
+ * vamos montar as conexões definindo as
+ * variáveis
+ */
+const int  enable_esqueda = 5,
+      input1_esquerda = 6,
+      input2_esquerda = 7,
+      reflexao_esquerda = 12,
+      enable_direita = 9,
+      input3_direita = 10,
+      input4_direita = 11,
+      reflexao_direita = 13;
+//escolhendo uma velocidade
+int velocidade = 255;      
+
+//tempo de espera para o Arduíno
+//em milissegundos
+int espera = 50;
+
+//definindo os pinos do Arduíno
+void setup(){
+  pinMode(enable_esquerda,OUTPUT);
+  pinMode(input1_esquerda,OUTPUT);
+  pinMode(input2_esquerda,OUTPUT);
+  pinMode(reflexao_esquerda,INPUT);
+  pinMode(enable_direita,OUTPUT);
+  pinMode(input3_direita,OUTPUT);
+  pinMode(input4_direita,OUTPUT);
+  pinMode(reflexao_direita,INPUT);
+}
+
+//Criando as funções que farão o
+//Arduíno andar (frente/direita/esquerda)
+void frente(){
+  analogWrite(enable_esquerda, velocidade);
+  analogWrite(enable_direita, velocidade);
+  //note que os comandos a seguir podem fazer
+  //seu motor girar ao contrário do esperado
+  digitalWrite(input1_esquerda, HIGH);
+  digitalWrite(input2_esquerda, LOW);
+  digitalWrite(input3_direita, HIGH);
+  digitalWrite(input4_direita, LOW);
+}
+
+void esquerda(){  
+  analogWrite(enable_esquerda, velocidade);
+  analogWrite(enable_direita, 0);
+  //note que os comandos a seguir podem fazer
+  //seu motor girar ao contrário do esperado
+  digitalWrite(input1_esquerda, HIGH);
+  digitalWrite(input2_esquerda, LOW);
+
+  digitalWrite(input3_direita, LOW);
+  digitalWrite(input4_direita, LOW);
+}
+
+void direita(){  
+  analogWrite(enable_esquerda, 0);
+  analogWrite(enable_direita, velocidade);
+  
+  digitalWrite(input1_esquerda, LOW);
+  digitalWrite(input2_esquerda, LOW);
+
+  //note que os comandos a seguir podem fazer
+  //seu motor girar ao contrário do esperado
+  digitalWrite(input3_direita, HIGH);
+  digitalWrite(input4_direita, LOW);
+}
+
+void parar(){   
+  analogWrite(enable_esquerda, 0);
+  analogWrite(enable_direita, 0);  
+  digitalWrite(input1_esquerda, LOW);
+  digitalWrite(input2_esquerda, LOW);
+  digitalWrite(input3_direita, LOW);
+  digitalWrite(input4_direita, LOW);
+}
+
+//agora o programa em loop
+void loop(){
+  //lendo os sensores
+  int sensorEsquerdo = digitalRead(reflexao_esquerda);
+  int sensorDireito = digitalRead(reflexao_direita);
+  //analisando os casos para seguir a linha
+  //quando no branco o sensor retorna 0
+  //quando no preto o sensor retorna 1
+  if(sensorEsquerdo == 0 && sensorDireito == 0){
+    frente();
+    delay(espera);
+  }
+  else if(sensorEsquerdo == 1 && sensorDireito == 0){
+    direita();
+    delay(espera);
+  }
+  else if(sensorEsquerdo == 0 && sensorDireito == 1){
+    esquerda();
+    delay(espera);
+  }
+  else {
+    parar();
+    delay(espera);
+  }
+}
